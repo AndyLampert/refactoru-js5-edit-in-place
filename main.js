069c1eca5d37edb53c6464652b56b1656e33a17e
AndyLampert <lamperta@gmail.com>
2014-04-09 18:51:42 -0600

    added hidden inputs

--- main.js
+++ main.js
@@ -2,19 +2,34 @@
 $(document).on('ready', function(){
 	$(".userInfo").on("click", function() {
 		$(this).hide();
-		$(this).after("<input class='input' type='text'><br>");
-		// $(this).on ("blur", function(){
-
-		// });
+		// when I hide, I want to pull the text and put it in the input
+		$(this).before().css(".userInput", "display:block");
 	});
-	$(".userInfo").on("mouseover", function(){
-		$(this).css("cursor", "pointer");
-	})
+
+
+// <input id='input-id' class='input' type='text'>
+// select the element and call .focus()
+
+
+$("hidden input").on("blur", function(){
+	var userInput = $("#input-id").val();
+	$(this).text("#input-id");
+			// get value with .val()
+			// get the text with .text()
+		});
 });
 
+// CSS is more declaritive/reusable. move to CSS
+// (rule of thumb is that if it can be done in CSS, do it in CSS)
+//	$(".userInfo").on("mouseover", function(){
+//		$(this).css("cursor", "pointer");
+//	});
+// });
 
-// When the input is blurred:
+
+// Ask Raine
+// How to get the input style to match the .userInfo block
+// get hints on how to finish step 4:
 // Hide the input.
 // Show the text block.
 // Update the text block with the new text.
-
